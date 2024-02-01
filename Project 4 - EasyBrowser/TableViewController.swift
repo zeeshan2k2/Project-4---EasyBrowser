abe8e27a44cb6dd5952fa9a88c3c3fe711bab06b
Zeeshan <100070393+zeeshan2k2@users.noreply.github.com>
2024-02-01 05:20:14 +0500

    first

---
+++ Project 4 - EasyBrowser/TableViewController.swift
@@ -0,0 +1,43 @@
+//
+//  TableViewController.swift
+//  Project 4 - EasyBrowser
+//
+//  Created by Zeeshan Waheed on 01/02/2024.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+
+    var websites = ["apple.com", "hackingwithswift.com", "google.com", "fast.com", "microsoft.com"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "Easy Browser"
+        
+
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return websites.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Websites", for: indexPath)
+        cell.textLabel?.text = "\(indexPath.row+1).  " + websites[indexPath.row]
+        return cell
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let vc = storyboard?.instantiateViewController(withIdentifier:"Webpage") as? ViewController {
+            vc.websites = websites
+            vc.SelectedSites = websites[indexPath.row]
+//            print(websites)
+            navigationController?.pushViewController(vc, animated: true)
+        }
+    }
+    
+
+}
--- Project 4 - EasyBrowser/ViewController.swift
+++ Project 4 - EasyBrowser/ViewController.swift
@@ -6,14 +6,111 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
 
+
+class ViewController: UIViewController, WKNavigationDelegate, UIAlertViewDelegate {
+
+    var webView: WKWebView!
+    var progressView: UIProgressView!
+//  array to store all websites
+//    var websites = ["apple.com", "hackingwithswift.com"]
+    var websites: [String]!
+    var SelectedSites: String!
+    
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
+        
+//      adding a refresh button and progress bar
+        let spacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webView.reload))
+        let back = UIBarButtonItem(image: UIImage(systemName: "chevron.backward"),
+                                   style: .plain,
+                                   target: webView,
+                                   action: #selector(webView.goBack))
+        let forward = UIBarButtonItem(image: UIImage(systemName: "chevron.forward"),
+                                      style: .plain,
+                                      target: webView,
+                                      action: #selector(webView.goForward))
+        progressView = UIProgressView(progressViewStyle: .default)
+        progressView.sizeToFit()
+        let progressButton = UIBarButtonItem(customView: progressView)
+        
+        toolbarItems = [progressButton, spacer, back, forward, spacer, refresh]
+        navigationController?.isToolbarHidden = false
+//      for progress bar
+        webView.addObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), options: .new, context: nil)
+        
+//      URL is the datatype for urls
+        let url = URL(string: "https://" + SelectedSites)!
+        webView.load(URLRequest(url: url))
+//      swipe left and right for previous page aur next page
+        webView.allowsBackForwardNavigationGestures = true
+        
+    }
+    
+//  open bar button functionality
+    @objc func openTapped() {
+        
+        let ac = UIAlertController(title: "Open page...", message: nil, preferredStyle: .actionSheet)
+        
+        for website in websites {
+            ac.addAction(UIAlertAction(title: website, style: .default, handler: openPage))
+        }
+        
+        ac.addAction(UIAlertAction(title: "cancel", style: .cancel))
+        ac.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(ac, animated: true)
     }
 
+    func openPage(action: UIAlertAction) {
+//        let url = URL(string: "https://" + action.title!)!
+        guard let actionTitle = action.title else { return }
+        guard let url = URL(string: "https://" + actionTitle) else { return }
+        webView.load(URLRequest(url: url))
+    }
 
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        title = webView.title
+    }
+    
+//  for progress bar to load (function correctly)
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == "estimatedProgress" {
+            progressView.progress = Float(webView.estimatedProgress)
+        }
+    }
+    
+    
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        let url = navigationAction.request.url
+        
+        if let host = url?.host {
+            for website in websites {
+                if host.contains(website) {
+                    decisionHandler(.allow)
+                    return
+                } else {
+                    let alert = UIAlertController(title: "Error", message: "Site is not safe", preferredStyle: .alert)
+//                    print("site wasn't safe")
+                    alert.addAction(UIAlertAction(title: "cancel", style: .cancel))
+                    decisionHandler(.cancel)
+                    present(alert, animated: true)
+                    return
+                }
+            }
+        }
+        
+        decisionHandler(.cancel)
+    }
 }
 
